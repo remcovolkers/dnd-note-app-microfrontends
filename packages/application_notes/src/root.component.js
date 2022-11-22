449b4b1ccd9829354b1dba5fbf861b69345df0a2
Remco Volkers <460318@student.saxion.nl>
2022-11-22 12:48:55 +0100

    better readability of "hello" very important push

--- packages/application_notes/src/root.component.js
+++ packages/application_notes/src/root.component.js
@@ -3,5 +3,5 @@ import React from "react";
 import "./styles/styles.scss";
 
 export default function Root(props) {
-  return <Container className="100vh vw-100 bg-primary">Hallo</Container>;
+  return <Container className="100vh vw-100 bg-secondary">Hallo</Container>;
 }
