7cd094c5ab48f4b2b65640f2288f8ad0222fc9fe
Remco Volkers <460318@student.saxion.nl>
2022-11-26 15:40:09 +0100

    not perfect

--- packages/notebook/src/root.component.js
+++ packages/notebook/src/root.component.js
@@ -4,7 +4,7 @@ import Container from "react-bootstrap/Container";
 
 export default function Root(props) {
   return (
-    <Container className="bg-primary" style={{ height: "90vh" }}>
+    <Container className="bg-primary vh-100">
       <Col className="text-light">
         <Row>Item 1</Row>
         <Row>Item 2</Row>
